1784fdb83048b50960ee659b8a1e9297edd61695
Cindy Unrau <cindy4th@hotmail.com>
2023-03-29 17:25:43 -0700

    Restructured Code to be more Object-Oriented

--- timelapse.py
+++ timelapse.py
@@ -1,168 +1,154 @@
 import os
-import requests
-from time import sleep
-from urllib.request import urlopen
+import shutil
 import logging
 import argparse
-import json
 from datetime import datetime
-import shutil
+import subprocess
+from time import sleep
+from requests.adapters import HTTPAdapter, Retry
+
+from api import Printer, PrintJob
+
+TEMP_DIR = "temp_files"
+OUT_DIR = "video_files"
+IMG_TYPE = ".jpg"
+IMAGE_FORMAT = "%05d" + IMG_TYPE
+VID_TYPE = ".mp4"
+FPS = 30
+VID_LENGTH = 30
 
 argParser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
 argParser.add_argument('NAME', type=str, 
     help='Name of the Ultimaker 3D Printer')
 argParser.add_argument('IP', type=str, 
     help='Name of the Ultimaker 3D Printer')
-
 args = argParser.parse_args()
 
-HTTP = "http://"
-SNAPSHOT = ":8080/?action=snapshot"
-PRINTER = "/api/v1/printer"
-PRINTER_STATUS = "/api/v1/printer/status"
-PRINT_JOB = "/api/v1/print_job"
-
-TEMP_DIR = "temp_files"
-OUT_DIR = "video_files"
-IMAGE_FORMAT = "%05d.jpg"
-
 NAME = args.NAME.upper()
-HOST = HTTP + args.IP
+IP = args.IP
 
-logging.basicConfig(filename= NAME.lower()+".log", filemode='a', format='%(asctime)s - %(levelname)s - ' + NAME + ' - %(message)s', 
+LOG_FILE = "asdf.log"
+
+logging.basicConfig(filename= LOG_FILE, filemode='a', format='%(asctime)s - %(levelname)s - ' + NAME + ' - %(message)s', 
                     datefmt='%d-%b-%y %H:%M:%S', level=logging.INFO)
 
-def connection_error(err):
-    logging.error('Connection Error: %s', err)
-    logging.error('Retrying in 5 seconds...')
-    sleep(5)
-
-def init_directory(directory_name):
-    dir = os.path.join(os.getcwd(),directory_name)
+def init_directory(name):
+    logging.info("Initializing Directory '%s'" % name)
+    dir = os.path.join(os.getcwd(), name)
     if not os.path.exists(dir):
         os.mkdir(dir)
-        print("Directory '% s' created" % dir) 
-    print("Directory '% s' exists" % dir)
+
     return dir
+    
+def save_image(path, image, count):
+    filename = os.path.join(path, IMAGE_FORMAT) % count
+    f = open(filename,'bw')
+    f.write(image)
+    f.close
 
-def get_status():
-    while True:
-        try:
-            res = requests.get(HOST + PRINTER_STATUS)
-            res.raise_for_status() 
-            return res.text.strip('\"') 
-        except Exception as e:
-            connection_error(e)
+def generate_timelapse(tmpdir, outdir, job_name):
+    date = datetime.now().strftime("%Y-%m-%d")
+    filename = f"{date}_{job_name.strip().replace(' ', '_')}_0{VID_TYPE}"
 
-def get_job():
-    res = requests.get(HOST + PRINT_JOB)
-    if(res.status_code == 404):
-        return None
-    return json.loads(res.content)
+    while os.path.isfile(f"{outdir}/{filename}"):
+        filename = f"{filename[:-2-len(VID_TYPE)]}_{int(filename[-1-len(VID_TYPE)])+1}{VID_TYPE}"
 
-def start_print(tmpdir, job_name):
-    logging.info('New Print Started:')
-    logging.info('Printer = %s, File = %s' % (NAME, job_name))
+    in_path = os.path.join(tmpdir, IMAGE_FORMAT)
+    out_path = f"{outdir}/{filename}"
 
-    logging.info('Saving images to temp directory %s', tmpdir)
-    if not os.path.exists(tmpdir):
-        os.makedirs(tmpdir)
+    logging.info(f"Starting Timelapse Generation: Saving Video as {filename}")
 
-def end_print(path,name):
-    logging.info('Print Done or Stopped') 
+    ffmpegcmd = ["ffmpeg", "-loglevel", "level+warning", "-r", str(FPS), "-i", in_path, out_path]
 
-    init_directory(OUT_DIR)
-
-    date = datetime.now()
-    out_file = f"{OUT_DIR}/{date.year}-{date.month}-{date.day}_{name.strip().replace(' ', '_')}_{date.microsecond}.mp4"
-    in_files = os.path.join(path,IMAGE_FORMAT)
-    logging.info("Starting Timelapse Generation")
-    logging.info(f"Saving Video as {out_file} to {in_files}")
-
-    ffmpegcmd = f"ffmpeg -r 30 -i {in_files} {out_file}"
     logging.info(f"Executing ffmpeg command: {ffmpegcmd}")
-    try:
-        os.system(ffmpegcmd)
-        pass
-    except Exception as e:
-        logging.error("\tError: %s - %s." % (e.filename, e.strerror))
+    subprocess.run(ffmpegcmd,check = True)
+
     logging.info("Done Encoding Video")
 
-    try:
-        logging.info(f"Deleting temp folder {path}")
-        shutil.rmtree(path)
-    except OSError as e:
-        logging.error("\tError: %s - %s." % (e.filename, e.strerror))
+def delete_temp_folder(path):
+    logging.info(f"Deleting temp folder {path}")
+    shutil.rmtree(path)
     logging.info(f"Finished deleting temp folder")
 
-    logging.info(f"Print {name} Finished")
+def start_print(tmpdir, job_str):
+    logging.info('New Print Started: %s' % job_str)
+    logging.info('Saving images to temp directory %s', tmpdir)
+    init_directory(tmpdir)
+
+def end_print(tmpdir,outdir,job_name):
+    logging.info('Print Done or Stopped') 
+
+    try:
+        generate_timelapse(tmpdir, outdir, job_name)
+    except OSError as e:
+        logging.error(f"Error Encoding Video: {e}")
+        return
     
-def save_image(path,count,job):
-    res = urlopen(HOST + SNAPSHOT)
-    filename = os.path.join(path,IMAGE_FORMAT) % count
-    f = open(filename,'bw')
-    f.write(res.read())
-    f.close
-    print("Image #: %05i Progress: %2.2f%%" % (count,job["progress"]), end='\r')
-    if job["time_total"] < 14400:  
-        sleep(15)
-    else:
-        sleep(30)
+    try:
+        delete_temp_folder(tmpdir)
+    except OSError as e:
+        logging.error("\tError: %s - %s." % (e.filename, e.strerror))
+
+    logging.info(f"Print {job_name} Finished")
 
 def main():
-    directory = init_directory(TEMP_DIR)
-    count = 0
-    job_path = None
+    print("Starting program Timelapse.py\n\tPrinter: %s\n\tIP: %s" % (NAME, IP))
+    print("All logs saved in %s\n" % LOG_FILE)
 
-    print('Starting Timelapse.py - NAME = \"%s\", HOST = %s' % (NAME, HOST))
-    logging.info('Starting Timelapse.py - NAME = \"%s\", HOST = %s' % (NAME, HOST))
+    logging.info('Starting program Timelapse.py - HOST: %s' % IP)
 
-    printer_status = get_status()
-    job = get_job()
-    job_state, job_uuid = (job["state"], job["uuid"]) if job != None else (None, None)
+    t_path = init_directory(TEMP_DIR)
+    o_path = init_directory(OUT_DIR)
 
-    if job_state != None:
-        logging.info('Initial Job: Name: %s UUID: %s Progress: %.2f%%', job["name"],job_uuid,job["progress"])
-        path = os.path.join(os.path.join(os.getcwd(),TEMP_DIR),job_uuid)
-        if os.path.exists(path):
-            images = [f for f in os.listdir(path) if os.path.isfile(os.path.join(path, f))]
-            count = int(max(images).split('.')[0])
-        job_path = os.path.join(directory, job_uuid)
+    num_images = (FPS * VID_LENGTH)
 
-        if printer_status == "printing" and job_state == "printing":
-            job_path = os.path.join(directory, job_uuid)
-            start_print(job_path,job["name"])
+    # session = requests.Session()
+    # retries = Retry(total = 5, backoff_factor = 0.5, status_forcelist = [429, 500, 502, 503, 504])
+    # session.mount('http://', HTTPAdapter(max_retries = retries))
+
+    printer = Printer(IP)
+
+    if(printer.status == "printing"):
+        curr_job = PrintJob(printer, t_path, num_images)
+        init_directory(curr_job.path)
+
+        if os.path.exists(curr_job.path):
+            curr_job.image_count = len(os.listdir(curr_job.path))
+
+        logging.info('Job on start-up: %s' % curr_job)
 
     while True:
-        printer_status_new = get_status()
-        job = get_job()
-        job_state_new, job_uuid_new = (job["state"], job["uuid"]) if job != None else (None, None)
+        prev_printer_status = printer.status
+        prev_job_state = curr_job.state
+        printer.update()
+        curr_job.update()
 
-        if printer_status_new != printer_status:
-            logging.info('Printer Status Change: %s -> %s', printer_status, printer_status_new)
-            printer_status = printer_status_new
+        if prev_printer_status != printer.status:
+            logging.info('Printer Status Change: %s -> %s', prev_printer_status, printer.status)
                 
-        if printer_status == "printing":
-            if job_state_new != job_state or job_uuid_new != job_uuid:
-                if job_state_new != job_state:
-                    logging.info('Job State Change: %s -> %s', job_state, job_state_new)
-                elif job_uuid_new != job_uuid:
-                    logging.info('Job UUID Change: %s -> %s', job_uuid, job_uuid_new)
+        if printer.status == "printing":
+            if prev_job_state != curr_job.state:
+                logging.info('Job State Change: %s -> %s', prev_job_state, curr_job.state)
 
-                job_state, job_uuid = job_state_new, job_uuid_new
-
-                if job_state == "pre_print":
-                    count = 0
-                    job_path = os.path.join(directory, job_uuid)
-                    start_print(job_path,job["name"])
+                if curr_job.state == "pre_print":
+                    curr_job = PrintJob(printer, t_path, num_images)
+                    start_print(curr_job.path,str(curr_job))
                 
-                if job_state == "post_print":
-                    end_print(job_path,job["name"])
+                if curr_job.state == "post_print":
+                    end_print(t_path,o_path,curr_job.name)
 
-            if job_state == "printing":
-                count += 1
-                save_image(job_path,count,job)
-
+            if curr_job.state == "printing":
+                print("Current Job:  %s" % str(curr_job), end='\r')
+                curr_job.increment_count()
+                save_image(curr_job.path, printer.req_snapshot(),curr_job.image_count)
+                sleep(curr_job.shoot_interval)
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        logging.error('Main Crashed. %s: %s', type(e).__name__, e)
+        print(f'Main Crashed. See {LOG_FILE} for info')
+    except KeyboardInterrupt as k:
+        logging.error('User Quit. %s', type(k).__name__)
+        print(f'User Quit. See {LOG_FILE} for info')
